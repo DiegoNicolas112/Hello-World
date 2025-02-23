6cafa7eeb7e3108e42105f6699b73f4dd372a757
Diego Aviles <daviles@konos.cl>
2025-02-23 18:39:38 -0300

    [ADD] Python: Operadores y Expresiones

---
+++ Python/4. Operadores y Expresiones/Operadores lógicos.py
@@ -0,0 +1,14 @@
+# Not Negación lógica
+not True
+not True == False
+# And Conjunción lógica (agrupa uniendo)
+True and True
+True and False
+# Or - Disyunción lógica (agrupa separando)
+True or True
+True or False
+False or True
+c = "OTRA COSA"
+c == "EXIT" or c == "FIN" or c == "SALIR"
+c = "Lector"
+c[0] == "H" or c[0] == "h"
---
+++ Python/4. Operadores y Expresiones/Operadores relacionales.py
@@ -0,0 +1,28 @@
+# Igual que
+3 == 2
+# Mayor que
+3 > 2
+# Menor o igual que
+3 <= 4
+# Asignando valores a variables también se puede.
+a = 10
+b = 5
+a > b
+# Es distinto de:
+b != a
+# Comparar cadenas
+"Hola" = "Hola"
+"Hola" != "Hola"
+# Guardando cadenas en una variable
+c = "Hola"
+c[0] == "H"
+# El último caracter es a?
+c[-1] == "a"
+# Aplicando a listas
+l1 = [0,1,2]
+l2 = [2,3,4]
+l1 == l2
+# La longitud de lista 1 es igual a longitud de lista 2?
+len(l1) == len(l2)
+# El último elemento de la L1 es igual a primer elemento de l2
+l1[-1] == l2[0]
